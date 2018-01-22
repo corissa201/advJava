73cef1f324e116ec8c2816e513eae9db1868ad90
Eric Knapp <eknapp@madisoncollege.edu>
2018-01-21 18:35:47 -0600

    EJK: Adding the missing interface in the java112.utilities package.

---
+++ src/java112/utilities/PropertiesLoader.java
@@ -0,0 +1,28 @@
+package java112.utilities;
+
+
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * @author Eric Knapp
+ *
+ */
+public interface PropertiesLoader{
+
+    default Properties loadProperties(String propertiesFilePath){
+        Properties properties = new Properties();
+        try {
+            properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return null
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+        return properties;
+    }
+}
+
