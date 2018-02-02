a339c218fb8863a126dd8ec98e2e5dbceed98632
Corissa Engel <cengel@madisoncollege.edu>
2018-02-01 19:45:57 -0600

    Added comments to FileAnalysis class directing what other methods I will need to add

--- src/java112/analyzer/FileAnalysis.java
+++ src/java112/analyzer/FileAnalysis.java
@@ -6,10 +6,28 @@ package java112.analyzer;
  */
 public class FileAnalysis {
     /**
-     * Constructor for FileAnalyzer
-     */
+    * Constructor for FileAnalyzer
+    */
     public FileAnalyzer() {
 
     }
 
+    public void analyze(String[] arguments) {
+
+       // If 1 argument entered the application will output a message to the
+       //command line asking for the right input and then terminate the program
+       if (arguments.length != 1) {
+          System.out.println("Please enter one argument on the command line");
+          return;
+      }
+      //Create an instance of each Analyzer class and assign them to their instance variables
+
+      //Open the input file.
+
+      //Loop through all the lines of the input file and generate individual tokens
+
+      //Pass generated tokens to all Analyzer instances via the processToken() method
+
+      //Call the generateOutputFile() method for each Analyzer class in a method named writeOutputFiles()
+    }
 }
