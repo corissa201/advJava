bc118083bc6232bfe38ca26c08902116ebdfeef9
Corissa Engel <cengel@madisoncollege.edu>
2018-02-01 19:21:06 -0600

    Adding start point of project 1. Driver class

---
+++ src/java112/analyzer/Driver.java
@@ -0,0 +1,15 @@
+package java112.;
+
+/**
+ * @author Corissa Engel
+ * class Driver
+ */
+public class Driver {
+    /**
+     * Constructor for Driver
+     */
+    public Driver() {
+
+    }
+
+}
