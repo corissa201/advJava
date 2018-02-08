f1f1e5abd9f6442b9c3dc0754e4704d8b71e128a
Corissa Engel <cengel@madisoncollege.edu>
2018-02-08 17:17:27 -0600

    Realized I added the incorrect steps for the FileSummaryAnalyzer class and deleleted to start new

--- src/java112/analyzer/FileSummaryAnalyzer.java
+++ src/java112/analyzer/FileSummaryAnalyzer.java
@@ -10,34 +10,6 @@ public class FileSummaryAnalyzer {
      */
     public FileSummaryAnalyzer() {
 
-    // Create the name of the application generating the report. Make up a name for your application.
-
-
-    // Create the name of the author of the application and their section day and time.
-
-
-    // Create  the email address of the author.
-
-
-    // Create the name of the text file that was analyzed.
-
-
-    // Create the date and time the file that was analyzed.
-
-
-    // Create the last modified date of the analyzed file.
-
-
-    // Create the size of the file in bytes.
-
-
-    // Create the file URI of the analyzed file.
-
-
-    // Create the total number of individual tokens in the document.
-
-
-    //Output summary information in a file named summary.txt
 
 
     }
