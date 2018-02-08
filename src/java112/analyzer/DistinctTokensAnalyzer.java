de2f28e085897251cd8e43a0902b42cd65dd893e
Corissa Engel <cengel@madisoncollege.edu>
2018-02-08 17:38:51 -0600

    Adding comments to classes to describe what the class will do

--- src/java112/analyzer/DistinctTokensAnalyzer.java
+++ src/java112/analyzer/DistinctTokensAnalyzer.java
@@ -4,6 +4,8 @@ package java112.analyzer;
  * @author Corissa Engel
  * class DistinctTokensAnalyzer,
  */
+
+ //Class will create the report of all distinct tokens
 public class DistinctTokensAnalyzer{
     /**
      * Constructor for DistinctTokensAnalyzer,
--- src/java112/analyzer/FileSummaryAnalyzer.java
+++ src/java112/analyzer/FileSummaryAnalyzer.java
@@ -4,6 +4,8 @@ package java112.analyzer;
  * @author Corissa Engel
  * class FileSummaryAnalyzer
  */
+
+ //Class will create summary report
 public class FileSummaryAnalyzer {
     /**
      * Constructor for FileSummaryAnalyzer
