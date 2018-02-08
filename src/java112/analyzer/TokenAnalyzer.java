977550fcd55b101b92da89a6163f2e133f6528ba
Corissa Engel <cengel@madisoncollege.edu>
2018-02-08 17:35:38 -0600

    Adding comment and two methods to TokenAnalyzer interface

--- src/java112/analyzer/TokenAnalyzer.java
+++ src/java112/analyzer/TokenAnalyzer.java
@@ -4,7 +4,15 @@ package java112.analyzer;
  * @author Corissa Engel
  * class TokenAnalyzer
  */
+//needs to be implemented by any class that performs an analysis
 public interface TokenAnalyzer {
 
+   void processToken(String token){
+
+   }
+
+   void generateOutputFile(String inputFilePath, String outputFilePath){
+
+   }
 
 }
