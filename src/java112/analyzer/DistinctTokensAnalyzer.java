47f78017a03111b6c37ca63a9d26f89a0694d76e
Corissa Engel <cengel@madisoncollege.edu>
2018-02-08 17:53:06 -0600

    added instance variable, get method to class and created instance of a TreeSet and assigned it to the distinctTokens variable

--- src/java112/analyzer/DistinctTokensAnalyzer.java
+++ src/java112/analyzer/DistinctTokensAnalyzer.java
@@ -5,13 +5,20 @@ package java112.analyzer;
  * class DistinctTokensAnalyzer,
  */
 
- //Class will create the report of all distinct tokens
+ //Class will create the report of all distinct tokens and implement the TokenAnalyzer interface
 public class DistinctTokensAnalyzer{
-    /**
-     * Constructor for DistinctTokensAnalyzer,
-     */
-    public DistinctTokensAnalyzer() {
+   /**
+    * Constructor for DistinctTokensAnalyzer, zero parameter
+    */
+   public DistinctTokensAnalyzer() {
+      //create an instance of a TreeSet and assigns it to the distinctTokens variable.
+      distinctTokens TreeSet = new distinctTokens();
+   }
 
-    }
+   private Set<String> distinctTokens;
+
+   public Set<String> getDistinctTokens() {
+      return distinctTokens;
+   }
 
 }
