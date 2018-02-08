8d79d3b6718c69471bf6770d42ce4b509a8d0049
Corissa Engel <cengel@madisoncollege.edu>
2018-02-08 17:09:59 -0600

    Adding comment steps  for the FileSummaryAnalyzer

--- src/java112/analyzer/FileSummaryAnalyzer.java
+++ src/java112/analyzer/FileSummaryAnalyzer.java
@@ -10,6 +10,34 @@ public class FileSummaryAnalyzer {
      */
     public FileSummaryAnalyzer() {
 
+    // Output the name of the application generating the report. Make up a name for your application.
+
+
+    // Output the name of the author of the application and their section day and time.
+
+
+    // Output the email address of the author.
+
+
+    // Output the name of the text file that was analyzed.
+
+
+    // Output the date and time the file that was analyzed.
+
+
+    // Output the last modified date of the analyzed file.
+
+
+    // Output the size of the file in bytes.
+
+
+    // Output the file URI of the analyzed file.
+
+
+    // Output the total number of individual tokens in the document.
+
+
+
     }
 
 }
