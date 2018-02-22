06d93fe65182792a4f425bea5a70f5ef2286d4e0
Corissa Engel <cengel@madisoncollege.edu>
2018-02-21 19:55:19 -0600

    I added additional information to the javadoc comments in the Driver class to include more detail.

--- src/java112/analyzer/Driver.java
+++ src/java112/analyzer/Driver.java
@@ -2,7 +2,9 @@ package java112.analyzer;
 
 
 /**
- * Driver class in the analyzer program
+ * Driver class in the analyzer program.
+ * The Driver class will call the main processing method of the main
+ * class, FileAnalysis, passing the command line arguments array to the method.
  *
  * @author     Corissa Engel
  * @version     1.1
@@ -10,7 +12,7 @@ package java112.analyzer;
 public class Driver {
 
     /**
-     * The main method that will run the project 1 program
+     * The main method that will run the project 1 program.
      * @param arguments The command line arguments.
      */
     public static void main(String[] arguments) {
