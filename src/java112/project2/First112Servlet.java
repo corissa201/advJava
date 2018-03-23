3a36a9ad7a73fd0f685bc6eac2c5fb04b782e8a0
Corissa Engel <cengel@madisoncollege.edu>
2018-03-22 21:51:31 -0500

    All code from the first servlet was lost in the last commit. I reverted to the file saved before the last commit and rebuilt.

---
+++ src/java112/project2/First112Servlet.java
@@ -0,0 +1,57 @@
+package java112.project2;
+
+
+import java.io.*;
+import java.util.*;
+
+import java112.utilities.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+
+/**
+ *  This is part of a lab and is the first servlet for the course.
+ *
+ *@author    CEngel
+ */
+@WebServlet(
+name = "first112Servlet",
+urlPatterns = {"/first"}
+) public class First112Servlet extends HttpServlet implements PropertiesLoader {
+
+    private Properties properties;
+
+
+    public void init() throws ServletException {
+        properties = loadProperties("/project2.properties");
+    }
+
+
+    /**
+     *  Handles HTTP GET requests.
+     *
+     *@param  request                   the HttpServletRequest object
+     *@param  response                   the HttpServletResponse object
+     *@exception  ServletException  if there is a Servlet failure
+     *@exception  IOException       if there is an IO failure
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // set the response type before sending data
+        PrintWriter out = response.getWriter();
+        out.print("<HTML>");
+        out.print("<HEAD><TITLE>Corissa Engel</TITLE></HEAD>");
+        out.print("<BODY>");
+        out.print("<h1>Advanced Java Spring 2018</h1>");
+        out.print("<img src=\"images/first112Image.png\"/>");
+        out.print("<p><a href=\"/java112\">Return to Home page</a></p>");
+        out.print("</BODY>");
+        out.print("</HTML>");
+        out.close();
+    }
+}
+
