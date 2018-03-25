5485f0e03f4a17c90da7f3d6ade8646aa0eaf8d0
Corissa Engel <cengel@madisoncollege.edu>
2018-03-24 20:35:17 -0500

    Added javaDoc comments and formatting to the TokenAnalyzer class.

--- src/java112/analyzer/TokenAnalyzer.java
+++ src/java112/analyzer/TokenAnalyzer.java
@@ -12,12 +12,15 @@ public interface TokenAnalyzer{
 
     /**
      * This method is used to process the token for all Analyzer classes.
+     *
      * @param token A list of all the tokens read from the file.
      */
     void processToken(String token);
+
     /**
      * This method is used to generate the output files for all Analyzer classes.
-     * @param inputFilePath The file path to the input file.
+     *
+     *@param inputFilePath The file path to the input file.
      */
     void generateOutputFile(String inputFilePath);
 }
