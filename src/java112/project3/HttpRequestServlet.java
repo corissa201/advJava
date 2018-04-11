7b5a08e8d9fba019a2a017daaee44c8597e45bc0
Corissa Engel <cengel@madisoncollege.edu>
2018-04-11 09:54:57 -0500

    Added javadoc comments to the HttpRequestServlet.

--- src/java112/project3/HttpRequestServlet.java
+++ src/java112/project3/HttpRequestServlet.java
@@ -1,37 +1,38 @@
 package java112.project3;
 
+
 import java.io.*;
-import java.util.*;
 import java.lang.*;
+import java.util.*;
 
 import javax.servlet.*;
-import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
 
 /**
+ * The HttpRequestServlet will assign all the HttpRequestData objects and foward
+ * to the project3Request jsp page.
  *
- *
- *@author    Corissa Engel
+ * @author      Corissa Engel
+ * @version     1.0
  */
 @WebServlet(
-    name = "requestServlet",
-    urlPatterns = { "/request-servlet" }
-)
-
-public class HttpRequestServlet extends HttpServlet {
+name = "requestServlet",
+urlPatterns = {"/request-servlet"}
+) public class HttpRequestServlet extends HttpServlet {
 
     /**
      *  Handles HTTP GET requests.
      *
      *@param  request               the HttpRequest
      *@param  response              the HttpResponse
-     *@exception  ServletException  if there is a general
-     *                              servlet exception
-     *@exception  IOException       if there is a general
-     *                              I/O exception
+     *@exception  ServletException  if there is a general servlet exception
+     *@exception  IOException       if there is a general I/O exception
+     *
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+    throws ServletException, IOException {
 
         HttpRequestData myHttpBean = new HttpRequestData();
 
@@ -55,10 +56,4 @@ public class HttpRequestServlet extends HttpServlet {
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
         dispatcher.forward(request, response);
     }
-
 }
-
-
-
-
-
