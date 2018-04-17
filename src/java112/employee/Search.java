b73bb977b2ddb3b75003a027f6096d9256905dd6
Corissa Engel <cengel@madisoncollege.edu>
2018-04-17 13:57:44 -0500

    Adding Search javaBean for project 4.

---
+++ src/java112/employee/Search.java
@@ -0,0 +1,108 @@
+package java112.employee;
+
+
+/**
+ *  This is a JavaBean class for the search
+ *
+ * @author       cEngel
+ * @version     1.0
+ */
+public class Search {
+
+    private String enteredSearchType;
+    private String enteredSearchTerm;
+    private ArrayList[] databaseQueryResult;
+    private boolean queryFoundEmployee;
+
+
+    /**
+     *  Constructor for the Search object
+     */
+    public Search() {
+        data = "default value";
+    }
+
+
+    /**
+     * Returns the value of enteredSearchType.
+     * @return enteredSearchType A string that holds the entered search type.
+     */
+    public String getEnteredSearchType() {
+        return enteredSearchType;
+    }
+
+
+    /**
+     * Sets the value of enteredSearchType.
+     * @param enteredSearchType The value to assign enteredSearchType.
+     */
+    public void setEnteredSearchType(String enteredSearchType) {
+        this.enteredSearchType = enteredSearchType;
+    }
+
+
+    /**
+     * Returns the value of enteredSearchTerm.
+     * @return enteredSearchTerm A string that holds the entered search term.
+     */
+    public String getEnteredSearchTerm() {
+        return enteredSearchTerm;
+    }
+
+
+    /**
+     * Sets the value of enteredSearchTerm.
+     * @param enteredSearchTerm The value to assign enteredSearchTerm.
+     */
+    public void setEnteredSearchTerm(String enteredSearchTerm) {
+        this.enteredSearchTerm = enteredSearchTerm;
+    }
+
+
+    /**
+     * Returns the value of databaseQueryResult.
+     * @return databaseQueryResult An ArrayList that holds the results from the
+     * database query.
+     */
+    public ArrayList[] getDatabaseQueryResult() {
+        return databaseQueryResult;
+    }
+
+
+    /**
+     * Sets the value of databaseQueryResult.
+     * @param databaseQueryResult The value to assign databaseQueryResult.
+     */
+    public void setDatabaseQueryResult(ArrayList[] databaseQueryResult) {
+        this.databaseQueryResult = databaseQueryResult;
+    }
+
+
+    /**
+     * Returns the value of queryFoundEmployee.
+     * @return queryFoundEmployee A boolean indicating whether or not the query
+     * found any employees.
+     */
+    public boolean getQueryFoundEmployee() {
+        return queryFoundEmployee;
+    }
+
+
+    /**
+     * Sets the value of queryFoundEmployee.
+     * @param queryFoundEmployee The value to assign queryFoundEmployee.
+     */
+    public void setQueryFoundEmployee(boolean queryFoundEmployee) {
+        this.queryFoundEmployee = queryFoundEmployee;
+    }
+
+
+    /**
+     * This method will add the employee object to the List of found Employee
+     * objects.
+     * @param employee A object of employee.
+     */
+    public void addFoundEmployee(Employee employee) {
+        databaseQueryResult.add(employee);
+    }
+}
