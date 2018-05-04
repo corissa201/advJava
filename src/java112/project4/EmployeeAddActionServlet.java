e7ad080ffadca984168969df8d9f20702707f281
Corissa Engel <cengel@madisoncollege.edu>
2018-05-03 20:18:07 -0500

    Adding code to the doPost in the Add Employee Action Servlet.

---
+++ src/java112/project4/EmployeeAddActionServlet.java
@@ -0,0 +1,52 @@
+package java112.project4;
+
+
+import java.io.*;
+import java.util.*;
+
+import java112.employee.*;
+import java112.utilities.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+
+/**
+ *
+ *
+ *@author    Corissa Engel
+ */
+@WebServlet(
+name = "EmployeeAddActionServlet",
+urlPatterns = {"/employeeAddAction-servlet"}
+) public class EmployeeAddActionServlet extends HttpServlet {
+
+    /**
+     *  Handles HTTP GET requests.
+     *
+     *@param  request               the HttpRequest
+     *@param  response              the HttpResponse
+     *@exception  ServletException  if there is a general
+     *                              servlet exception
+     *@exception  IOException       if there is a general
+     *                              I/O exception
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException{
+
+        ServletContext context = request.getServletContext();
+
+        EmployeeDirectory empDirectory = (EmployeeDirectory) context.getAttribute("directory");
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String socialSecurityNumber = request.getParameter("socialSecurityNumber");
+        String department = request.getParameter("department");
+        String roomNumber = request.getParameter("roomNumber");
+        String phoneNumber = request.getParameter("phoneNumber");
+
+        empDirectory.addNewEmployeeRecord(firstName, lastName, socialSecurityNumber,
+        department, roomNumber, phoneNumber);
+    }
+}
