affedd5781cff93f4d7e91e36897a3bb7547e184
Corissa Engel <cengel@madisoncollege.edu>
2018-05-10 12:15:29 -0500

    Removing comments and cleaning up EmployeeSearchServlet.

--- src/java112/project4/EmployeeSearchServlet.java
+++ src/java112/project4/EmployeeSearchServlet.java
@@ -1,23 +1,27 @@
 package java112.project4;
 
+
 import java.io.*;
 import java.util.*;
 
 import java112.employee.*;
 
 import javax.servlet.*;
-import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
 
 /**
- *
+ * The EmployeeSearchServlet is only used to forward to the employeeSearch
+ * jsp page.
  *
  * @author    Corissa Engel
  * @version   1.0
+ *
  */
 @WebServlet(
-    name = "EmployeeSearch-Servlet",
-    urlPatterns = {"/employeeSearch-servlet"}
+name = "EmployeeSearch-Servlet",
+urlPatterns = {"/employeeSearch-servlet"}
 )
 
 public class EmployeeSearchServlet extends HttpServlet {
@@ -27,36 +31,17 @@ public class EmployeeSearchServlet extends HttpServlet {
      *
      *@param  request               the HttpRequest
      *@param  response              the HttpResponse
-     *@exception  ServletException  if there is a general
-     *                              servlet exception
+     *@exception  ServletException  if there is a general servlet exception
      *@exception  IOException       if there is a general
      *                              I/O exception
      */
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+    throws ServletException, IOException {
 
-        //ServletContext context = getServletContext();
-
-        //context.getAtribute(directory);
-
-        //String  searchTerm = request.getParameter("searchTerm");
-
-        //if (searchTerm == null || searchTerm.isEmpty()){
         String url = "/employeeSearch.jsp";
 
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
         dispatcher.forward(request, response);
-        }
-
-        //System.out.println("searchTerm: " + searchTerm);
-
-        //String  param2 = request.getParameter("param2");
-
-        //System.out.println("param2: " + param2);
-
     }
-
-
-
-
-
+}
