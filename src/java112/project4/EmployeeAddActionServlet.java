fc73ba60bb17b229f8c98aa25fcd974b45aad7d4
Corissa Engel <cengel@madisoncollege.edu>
2018-05-10 10:24:51 -0500

    Added if statement to the add action servlet to valida all form fields filled in.

--- src/java112/project4/EmployeeAddActionServlet.java
+++ src/java112/project4/EmployeeAddActionServlet.java
@@ -42,6 +42,7 @@ urlPatterns = {"/employeeAddAction-servlet"}
 
         EmployeeDirectory directory = (EmployeeDirectory)context.getAttribute("project4Directory");
 
+
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String socialSecurityNumber = request.getParameter("socialSecurityNumber");
@@ -49,10 +50,18 @@ urlPatterns = {"/employeeAddAction-servlet"}
         String roomNumber = request.getParameter("roomNumber");
         String phoneNumber = request.getParameter("phoneNumber");
 
-        String message = directory.addNewEmployeeRecord(firstName, lastName, socialSecurityNumber,
-        department, roomNumber, phoneNumber);
+        if (firstName.equals("") || lastName.equals("") || socialSecurityNumber.equals("") ||
+        department.equals("") || roomNumber.equals("") || phoneNumber.equals("")) {
+            String noValueEntered = "Employee information not added. Please fill in all form fields";
 
-        session.setAttribute("project4message", message);
+            session.setAttribute("project4message", noValueEntered);
+
+        } else {
+            String message = directory.addNewEmployeeRecord(firstName, lastName, socialSecurityNumber,
+            department, roomNumber, phoneNumber);
+
+            session.setAttribute("project4message", message);
+        }
 
 
         String url = "/java112/addNewEmployee-servlet";
